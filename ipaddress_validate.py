48a061419d82f1a877f4955f99a76f875be55f7e
Atul Jain <jatuljain@gmail.com>
2026-02-10 19:10:03 +0100

    To Check webhook

--- ipaddress_validate.py
+++ ipaddress_validate.py
@@ -1,3 +1,4 @@
+#To check if the given IP address is valid or not, we can use the ipaddress module in Python. The ipaddress module provides a way to create, manipulate and operate on IPv4 and IPv6 addresses and networks. We can use the ip_address function from the ipaddress module to check if the input is a valid IP address. If the input is not a valid IP address, it will raise a ValueError exception, which we can catch and handle accordingly.
 import ipaddress
 
 def checkipv4(ip):
@@ -10,21 +11,21 @@ def checkipv4(ip):
 	except ValueError:
 		# If the input is not a valid IP address, catch the exception and print an error message
 		return "Invalid IPv4 address"
-	
+
 def checkipv6(ip):
 	# Use the ip_address function from the ipaddress module to check if the input is a valid IPv6 address
 	try:
 		ip= ip.split('/')
-		print(ip[0])		
+		print(ip[0])
 		return ipaddress.IPv6Address(ip[0])
 		# print("Valid IPv6 address")
 	except ValueError:
 		# If the input is not a valid IP address, catch the exception and print an error message
 		return "Invalid IPv6 address"
 		print("This will never be called")
-	
-	
-	
+
+
+
 
 # Driver Code
 if __name__ == '__main__':
@@ -32,9 +33,9 @@ if __name__ == '__main__':
 	value = checkipv4(ip)
 	print("IPv4:", value)
 
-	ip = "3.1.2.2"	
+	ip = "3.1.2.2"
 	value = checkipv4(ip)
-	print("IP: ", value)	
+	print("IP: ", value)
 
 	ip = "2002:db8::/55"
 	value =  checkipv6(ip)
